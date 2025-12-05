fcdb36baea82501be2e06fef387ac2f6ef900615
BarryBonB2 <fdonovan1280@student.pps.net>
2025-12-05 15:32:13 -0800

    player collides with blocks now

--- scripts/purpleman.gd
+++ scripts/purpleman.gd
@@ -1,3 +1,5 @@
 extends CharacterBody2D
 
 @onready var yellowman: Yellow = $"../Yellowman"
+@export var maxHealth : int = 3
+@export var health : int = maxHealth
--- scripts/yellowman.gd
+++ scripts/yellowman.gd
@@ -4,10 +4,17 @@ class_name Yellow
 @onready var purpleman: CharacterBody2D = $"../Purpleman"
 
 @export var move_speed: float = 400.0
-var gravity: float= 2000
+@export var maxHealth : int = 1
+@export var health : int = maxHealth
+const GRAVITY = 200.0
 
-func _physics_process(_delta):
+
+func _physics_process(delta):
 	handle_movement()
+	#velocity.y += delta * GRAVITY
+	
+	#var motion = velocity * delta
+	#move_and_collide(Vector2(0, 1)) # Move down 1 pixel per physics frame
 
 func handle_movement():
 	# Get input direction from arrow keys
